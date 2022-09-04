8989dc659a0472f81bed861f8fc132c2a240b190
farbowski <111902880+farbowski@users.noreply.github.com>
2022-09-04 12:58:47 -0500

    Google

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,16 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-548Y5MVNXD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-548Y5MVNXD');
+</script>
+
       <main className={styles.main}>
         <header className={styles.header}>
           <Image
